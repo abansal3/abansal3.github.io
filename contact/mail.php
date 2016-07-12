5099c1cc851ff5392016ae3173cc47598743fece
Arpit Bansal <abansal3@babson.edu>
2016-07-12 21:50:52 +0530

    Modified contact/mail.php

--- contact/mail.php
+++ contact/mail.php
@@ -10,19 +10,20 @@
 
         // email config
 
-        $transport = Swift_SmtpTransport::newInstance('smtp.gmail.com', 465, "ssl")
+        $gmail = 'smtp.gmail.com';
+
+        $transport = Swift_SmtpTransport::newInstance($gmail, 587, "tls")
           ->setUsername('barpit123@gmail.com')
-          ->setPassword('vibgyor123');
+          ->setPassword('kvhncsdcbmckkhlo');
 
         $mailer = Swift_Mailer::newInstance($transport);
 
         $message = Swift_Message::newInstance('Arpit Bansal - Reaching out')
           ->setFrom(array('barpit123@gmail.com' => 'Arpit Bansal'))
-          ->setTo(array($email))
+          ->setTo(array($email,"abansal3@babson.edu"))
           ->setBody("Hi " . $name . ", \n\n" . "You had contacted me through my website. Just to verify, this is the information you had entered: \n\n" . "Name: $name\nEmail:$email\nPhone: $phone\nMessage: $message\n\nThank you so much for taking the time to explore my website! I will be reaching out to you personally to talk with you.");
 
         $result = $mailer->send($message);
-
     }
 
 ?>
