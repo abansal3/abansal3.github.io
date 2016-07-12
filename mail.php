4a9de8185e85daea30fc83a721994cde04f8340d
Arpit Bansal <abansal3@babson.edu>
2016-07-12 13:07:15 +0530

    Testing mail.php #5

--- mail.php
+++ mail.php
@@ -2,32 +2,28 @@
 
 require_once('vendor/swiftmailer/swiftmailer/lib/swift_required.php');
 
-    if (count(file_get_contents("php://input")) > 0) {
-        // Convert from json to object
-        $json = json_decode(file_get_contents("php://input"));
-        //print_r($json);
+    // Convert from json to object
+    $obj = json_decode(file_get_contents("php://input"));
 
-        // Assign variables
-        $name = $json->name;
-        $message = $json->message;
-        $phone = $json->phone;
-        $email = $json->email;
+    // Assign variables
+    $name = $obj->name;
+    $message = $obj->message;
+    $phone = $obj->phone;
+    $email = $obj->email;
 
-        // email config
+    // email config
 
-        $transport = Swift_SmtpTransport::newInstance('smtp.gmail.com', 587, "tls")
-          ->setUsername('barpit123@gmail.com')
-          ->setPassword('vibgyor123');
+    $transport = Swift_SmtpTransport::newInstance('smtp.gmail.com', 587, "tls")
+      ->setUsername('barpit123@gmail.com')
+      ->setPassword('vibgyor123');
 
-        $mailer = Swift_Mailer::newInstance($transport);
+    $mailer = Swift_Mailer::newInstance($transport);
 
-        $message = Swift_Message::newInstance('Arpit Bansal - Reaching out')
-          ->setFrom(array('barpit123@gmail.com' => 'Arpit Bansal'))
-          ->setTo(array($email))
-          ->setBody("Hi " . $name . ", \n\n" . "You had contacted me through my website. Just to verify, this is the information you had entered: \n\n" . "Name: $name\nEmail:$email\nPhone: $phone\nMessage: $message\n\nThank you so much for taking the time to explore my website! I will be reaching out to you personally to talk with you.");
+    $message = Swift_Message::newInstance('Arpit Bansal - Reaching out')
+      ->setFrom(array('barpit123@gmail.com' => 'Arpit Bansal'))
+      ->setTo(array($email))
+      ->setBody("Hi " . $name . ", \n\n" . "You had contacted me through my website. Just to verify, this is the information you had entered: \n\n" . "Name: $name\nEmail:$email\nPhone: $phone\nMessage: $message\n\nThank you so much for taking the time to explore my website! I will be reaching out to you personally to talk with you.");
 
-        $result = $mailer->send($message);
-
-    }
+    $result = $mailer->send($message);
 
 ?>
