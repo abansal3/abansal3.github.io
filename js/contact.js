1c271eda36eef86bc60707556f6b504dff1af895
Arpit Bansal <abansal3@babson.edu>
2017-05-06 17:12:16 -0400

    revamping website day1. changed landing page, added story section and foundation for work section

--- js/contact.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-    
-    // Click Bars for Dropdown
-    
-    $('.fa').click(function () {
-        $('.dropdown').css("visibility","visible").toggle();
-    });
-    
-});
